f1165c057ecd21c53b8f35780efe21220913ecf0
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-04 22:28:16 +0530

    Try to add Ai API

---
+++ src/utils/cohere.js
@@ -0,0 +1,15 @@
+
+
+import cohere from 'cohere-ai';
+import dotenv from 'dotenv';
+dotenv.config({path: './.env'});
+
+cohere.init(process.env.COHERE_API_KEY);
+
+const generateSummary = async (promptText) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+}
