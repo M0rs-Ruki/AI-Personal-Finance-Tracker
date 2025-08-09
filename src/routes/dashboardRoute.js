bdc53d16dcc13ac2c89407e613aa5fff97c7a045
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-10 01:38:10 +0530

    Fixed an error

--- src/routes/dashboardRoute.js
+++ src/routes/dashboardRoute.js
@@ -4,7 +4,7 @@ import express from "express";
 import { GuestUserDashboard } from "../controllers/guestController.js";
 import { StudentDashboard } from "../controllers/studentController.js";
 import { EmployerDashboard } from "../controllers/employedController.js";
-import { UnEmployedDashboard } from "../controllers/unemployedController.js";
+import { UnEmployedDashboard } from "../controllers/unEmployedController.js";
 import { RetiredDashboard } from "../controllers/retiredController.js";
 import isLoggedIn from "../middlewares/isLoggedInMiddleware.js";
 import {log} from 'console';
